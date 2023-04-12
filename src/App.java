6c9dd3d92cb2b64a1e46c0831e9940b9adae0f9e
Enzo Silveira <Enzo.Silveira@edu.pucrs.br>
2023-04-12 17:30:44 -0300

    Erros de lógica, inicialização das estruturas

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Processo proc = new Processo("prog2.txt", 0, 0);
+        proc.imprimeInstr();
     }
 }
--- src/Processo.java
+++ src/Processo.java
@@ -12,11 +12,26 @@ public class Processo {
     private ArrayList<String> instrucoes;
     private HashMap<String, Integer> dados;//A string do dado com seu valor em Integer
     private HashMap<String, Integer> labels;//Quando ler o arquivo a label vai ser o tamanho(ou seja, o indice do proximo a ser escrito)
+    private int id, prio;
 
-    public Processo(String narq){
+    public Processo(String narq, int id, int prio){
+        dados = new HashMap<>();
+        labels = new HashMap<>();
+        instrucoes = new ArrayList<>();
         criarProcesso(narq);
+        this.id = id;
+        this.prio = prio;
+       
     };
-    //metodo void que coloca nos dicionarios de dados e de labels os dados e no array de instruções os dados
+  
+    public int getPrio(){
+        return this.prio;
+    }
+    public void imprimeInstr(){
+        for (String string : instrucoes) {
+            System.out.println(string);
+        }
+    }
 
     private void criarProcesso (String naqr){
         boolean data = false;
@@ -27,8 +42,11 @@ public class Processo {
             try (Scanner sc = new Scanner(Files.newBufferedReader(path2, Charset.defaultCharset()))){
                 while(sc.hasNextLine()) {
                     String lines = sc.nextLine();
+                    String line = lines;
+                    if (lines.contains("#")){
                     int commentPos = lines.indexOf("#");
-                    String line = lines.substring(0, commentPos);
+                    line = lines.substring(0, commentPos);
+                    } 
                     if(line.equals(".code")){
                         code = true;
                     }
